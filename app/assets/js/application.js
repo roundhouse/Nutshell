bc747e81dbf6b7a0c66bd4f03981e6f2e9800f8c
Vadim Goncharov <vadim@owl-design.net>
2015-08-13 14:42:56 -0700

    code fix

--- app/assets/js/application.js
+++ app/assets/js/application.js
@@ -1,14 +1,24 @@
-var example, loadApplication;
+var App,
+  bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
-loadApplication = function() {
-  var styles;
-  styles = ["display: block", "background: #f7cd81", "color: white", "padding: 20px 20px 20px 20px", "text-align: center", "font-weight: normal", "font-size: 20px", "line-height: 60px"].join(';');
-  console.log('%c Nutshell!', styles, 'Has loaded.');
-  return $(example);
-};
+App = (function() {
+  function App() {
+    this.init = bind(this.init, this);
+  }
 
-example = function() {
-  return console.log('Happy Coding!');
-};
+  App.prototype.init = function() {
+    var styles;
+    styles = ["display: block", "background: #f7cd81", "color: white", "padding: 20px 20px 20px 20px", "text-align: center", "font-weight: normal", "font-size: 20px", "line-height: 60px"].join(';');
+    console.log('%c Nutshell!', styles, 'Has loaded.');
+    return console.log('Happy Coding!');
+  };
 
-$(loadApplication);
+  return App;
+
+})();
+
+$(function() {
+  var Application;
+  Application = new App();
+  return Application.init();
+});
--- dev/coffee/application.coffee
+++ dev/coffee/application.coffee
@@ -1,10 +1,9 @@
-loadApplication = ->
-  styles = ["display: block","background: #f7cd81","color: white","padding: 20px 20px 20px 20px","text-align: center","font-weight: normal","font-size: 20px","line-height: 60px"].join(';')
-  console.log '%c Nutshell!', styles, 'Has loaded.'
-  $(example)
+class App
+  init: =>
+    styles = ["display: block","background: #f7cd81","color: white","padding: 20px 20px 20px 20px","text-align: center","font-weight: normal","font-size: 20px","line-height: 60px"].join(';')
+    console.log '%c Nutshell!', styles, 'Has loaded.'
+    console.log 'Happy Coding!'
 
-example = ->
-  console.log 'Happy Coding!'
-
-
-$(loadApplication)
+$ ->
+  Application = new App()
+  Application.init()
